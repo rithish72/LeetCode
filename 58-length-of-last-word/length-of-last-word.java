3ea8492ec0615b112f3ca86322183bec6a869c2b
Kurapati Rithish <120491962+rithish72@users.noreply.github.com>
2024-04-01 10:57:27 +0530

    Time: 0 ms (100.00%) | Memory: 41.8 MB (30.67%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,15 +1,11 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        s=s.trim();
-        
-        for(int i=s.length()-1;i>=0;i--){
-            if(s.charAt(i) == ' '){
-                System.out.println(s);
-                return (s.length()-i-1);
-            }
-                
+        int count = 0;
+        for(int i=s.length()-1; i>=0; i--){
+            if(s.charAt(i) == ' ' && count == 0)continue;
+            else if(s.charAt(i) == ' ') return count;
+            else count++;
         }
-
-        return s.length();
+        return count;
     }
 }
