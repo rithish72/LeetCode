a1f2a23d062f24010a15ea01cac95d54da787560
Kurapati Rithish <120491962+rithish72@users.noreply.github.com>
2024-04-05 13:15:38 +0530

    Time: 56 ms (31.72%) | Memory: 49.6 MB (25.38%) - LeetSync

--- 2864-is-object-empty/is-object-empty.js
+++ 2864-is-object-empty/is-object-empty.js
@@ -3,7 +3,5 @@
  * @return {boolean}
  */
 var isEmpty = function(obj) {
-    for(let key in obj)
-        return false;
-    return true;
+     return Object.keys(obj).length === 0 || obj === []
 };
