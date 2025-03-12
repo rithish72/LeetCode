41e0a128eb88ec04ef7704366d5cbc2dbaba3db0
Kurapati Rithish <120491962+rithish72@users.noreply.github.com>
2025-03-12 21:59:05 +0530

    Time: 1 ms (47.70%) | Memory: 56.4 MB (6.05%) - LeetSync

---
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumCount = function(nums) {
+    let pos = 0;
+    let neg = 0;
+    for(let i=0;i<nums.length;i++){
+        if(nums[i] < 0){
+            neg++;
+        }
+        else if(nums[i] > 0){
+            pos++;
+        }
+    }
+    return Math.max(pos,neg);
+};
