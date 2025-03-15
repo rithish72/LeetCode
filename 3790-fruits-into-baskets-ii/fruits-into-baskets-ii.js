ec635bc070f0acf452c56f514e50301db6dd435b
Kurapati Rithish <120491962+rithish72@users.noreply.github.com>
2025-03-15 18:18:22 +0530

    Time: 1 ms (91.01%) | Memory: 57.5 MB (36.24%) - LeetSync

---
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} fruits
+ * @param {number[]} baskets
+ * @return {number}
+ */
+var numOfUnplacedFruits = function(fruits, baskets) {
+    let unplaced = 0;
+    let n = fruits.length;
+    let m = baskets.length;
+
+    for(let i=0;i<n;i++){
+        let placed = false;
+        for(let j=0;j<m;j++){
+            if(baskets[j] >= fruits[i]){
+                baskets[j] = -1;
+                placed = true;
+                break;
+            }
+        }
+        if(!placed){
+            unplaced++;
+        }
+    }
+    return unplaced;
+};
